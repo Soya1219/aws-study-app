ccb5817e9e95391736222447d8857eed9b3b5a65
Soya1219 <s2220235@stu.musashino-u.ac.jp>
2026-02-03 04:15:52 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'aws-master-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './data.json',
+  './ogp.png'
+];
+
+// インストール時にファイルをキャッシュする
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// リクエスト時にキャッシュがあればそれを返す（オフライン対応）
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
